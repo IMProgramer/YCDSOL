98115f1d41f4be83723af82be7d026cc1134afff
Yash Kansagara <yash.kansagara@hotmail.com>
2013-08-27 17:19:26 +0530

    Doubly Linked List: insert,delete,print

--- LinkedList/dll.c
+++ LinkedList/dll.c
@@ -29,6 +29,38 @@ void insert_first(int data){
 	}
 }
 
+void delete(int data){
+	printf("delete(%d)\n",data);
+	if(first==NULL)
+		return;
+	node *n;
+	for(n = first ; (n != NULL) && (n->data != data) ; n = n->next);
+	
+	// Only 1 node
+	if(first==last){
+		first = NULL;
+		last = NULL;
+	// first node
+	}else if(n==first){
+		first = first->next;
+		first->pre = NULL;
+		free(n);	
+		
+	// last node
+	}else if(n==last){
+		last = last->pre;
+		last->next = NULL;
+		free(n);
+	
+	// middle node
+	}else{
+		n->pre->next = n->next;
+		n->next->pre = n->pre;
+		free(n);
+	}
+	
+}
+
 void insert_after(int match,int data){
 	printf("insert_after(%d,%d)\n",match,data);
 	if(first==NULL){
@@ -53,7 +85,45 @@ void insert_after(int match,int data){
 	}
 }
 
+void insert_before(int match,int data){
+	printf("insert_before(%d,%d)\n",match,data);
+	if(first==NULL){
+		insert_first(data);
+	}else{
+		node *new = (node*)malloc(sizeof(node));
+		new->data = data;
+		node *find=first;
+		
+		while(find != NULL && find->data != match)
+			find = find->next;
+		
+		new->next = find;
+		new->pre = find->pre;
+		
+		if(find->pre == NULL)
+			first = new;
+		else
+			find->pre->next = new;
+		find->pre = new;
+		
+	}
+}
 
+
+void insert_end(int data){
+	printf("insert_end(%d)\n",data);
+	if(first == NULL){
+		insert_first(data);
+	}else{
+		node *new = (node*)malloc(sizeof(node));
+		new->data = data;
+		new->pre = last;
+		last->next = new;
+		new->next = NULL;
+		last = new;
+		
+	}
+}
 void print_r(){
 	if(last==NULL)
 		printf("No data");
@@ -86,6 +156,8 @@ void print(){
 	printf("\n");
 }
 void main(){
+	print();
+	print_r();
 	insert_first(3);
 	print();
 	print_r();
@@ -95,7 +167,22 @@ void main(){
 	insert_after(1,2);	
 	print();
 	print_r();
-	insert_after(3,4);
+	insert_after(3,5);
 	print();
-	print_r();		
+	print_r();
+	insert_before(5,4);
+	print();
+	print_r();	
+	insert_end(6);
+	print();
+	print_r();
+	delete(1);
+	print();
+	print_r();
+	delete(6);
+	print();
+	print_r();
+	delete(4);
+	print();
+	print_r();	
 }
